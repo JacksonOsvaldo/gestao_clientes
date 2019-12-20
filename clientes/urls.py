e2d821e95f7092819a523d081b663dd6197987d1
Leila <leilaapsilva26@gmail.com>
2019-12-20 12:22:30 -0300

    Utilizando Bulk Create

--- clientes/urls.py
+++ clientes/urls.py
@@ -8,6 +8,7 @@ from .views import PersonDetail
 from .views import PersonCreate
 from .views import PersonUpdate
 from .views import PersonDelete
+from .views import ProdutoBulk
 
 urlpatterns = [
     path('list/', persons_list, name="person_list"),
@@ -19,4 +20,5 @@ urlpatterns = [
     path('person_update/<int:pk>/', PersonUpdate.as_view(), name='person_update_cbv'),
     path('person_delete/<int:pk>/', PersonDelete.as_view(), name='person_delete_cbv'),
     path('person_create/', PersonCreate.as_view(), name='person_create_cbv'),
+    path('person_bulk/', ProdutoBulk.as_view(), name='person_bulk'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Person
+from django.views.generic.base import View
+
+from .models import Person, Produto
 from .forms import PersonForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -67,3 +70,17 @@ class PersonDelete(DeleteView):
 
     def get_success_url(self):
         return reverse_lazy('person_list_cbv')
+
+class ProdutoBulk(View):
+    def get(self, request):
+        produtos = ['Banana', 'Maca', 'Limao', 'Laranja', 'Pera', 'Melancia']
+        list_produtos = []
+
+        for produto in produtos:
+            p = Produto(descricao=produto, preco=10)
+            list_produtos.append(p)
+
+        Produto.objects.bulk_create(list_produtos)
+
+        return HttpResponse('Funcionou')
+
